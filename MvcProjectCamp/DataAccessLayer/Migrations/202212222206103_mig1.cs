3ab5730ef78936f2c22801d2fc03471dfc3ca462
Cengiz ERYİĞİT <cengiz_eryigit@hotmail.com>
2022-12-23 13:47:14 +0300

    MVC Project Camp Video 20
    MVC Project Camp Video 20

---
+++ MvcProjectCamp/DataAccessLayer/Migrations/202212222206103_mig1.cs
@@ -0,0 +1,18 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig1 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Contents", "ContentValue", c => c.String(maxLength: 1001));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Contents", "ContentValue", c => c.String(maxLength: 1000));
+        }
+    }
+}
